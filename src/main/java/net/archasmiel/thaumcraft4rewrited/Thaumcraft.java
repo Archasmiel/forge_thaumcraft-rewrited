cee3a271d87cf727b44cfa78a1f9870c2ed38a02
Archasmiel <s.remez-me23@lll.kpi.ua>
2022-06-30 09:51:42 +0300

    [v0.0.0.1 upd.1] - updated to forge 1.19

---
+++ src/main/java/net/archasmiel/thaumcraft4rewrited/Thaumcraft.java
@@ -0,0 +1,47 @@
+package net.archasmiel.thaumcraft4rewrited;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Thaumcraft.MOD_ID)
+public class Thaumcraft {
+
+    public static final String MOD_NAME = "Thaumcraft 4 Rewrited";
+    public static final String MOD_ID = "thaumcraft4rewrited";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Thaumcraft() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
