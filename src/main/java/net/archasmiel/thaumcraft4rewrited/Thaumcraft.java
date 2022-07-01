f97f56bbd02748a18ccbd09028c77c4f4f5d9123
Archasmiel <s.remez-me23@lll.kpi.ua>
2022-07-01 09:54:44 +0300

    [v0.0.1] - returned to forge 1.18.2

--- src/main/java/net/archasmiel/thaumcraft4rewrited/Thaumcraft.java
+++ src/main/java/net/archasmiel/thaumcraft4rewrited/Thaumcraft.java
@@ -1,47 +1,45 @@
 package net.archasmiel.thaumcraft4rewrited;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+import java.util.stream.Collectors;
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Thaumcraft.MOD_ID)
-public class Thaumcraft {
+public class Thaumcraft
+{
 
     public static final String MOD_NAME = "Thaumcraft 4 Rewrited";
     public static final String MOD_ID = "thaumcraft4rewrited";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public Thaumcraft() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
-
+    public Thaumcraft()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-
-        }
-    }
 }
